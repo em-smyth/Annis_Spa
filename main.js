e63377a7ed5abeebb8a5512fe7fa2c899f8db1aa
Emma <105153833+em-smyth@users.noreply.github.com>
2022-08-18 23:01:07 +0100

    reveal elements on scroll

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Carousel
+
 document.querySelectorAll(".carousel").forEach((carousel) => {
   const items = carousel.querySelectorAll(".carousel-item");
   console.log("items", items);
@@ -8,9 +10,9 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
   carousel.insertAdjacentHTML(
     "beforeend",
     `
-  <div class="carousel-nav">
-  ${buttonsHtml.join("")}
-  `
+    <div class="carousel-nav">
+    ${buttonsHtml.join("")}
+    `
   );
 
   const buttons = carousel.querySelectorAll(".carousel-button");
@@ -53,4 +55,96 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
 
 function addLeadingZeros(num, totalLength) {
   return String(num).padStart(totalLength, "0");
+}
+
+// Reveal Elements on Scroll and load from below
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    let revealpoint = 150;
+
+    // console.log("element number: " + i);
+    // console.log(`window Height: ${windowheight - revealpoint}`);
+    // console.log("revealtop: " + revealtop);
+    // console.log("=================================== ");
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("active");
+    } else {
+      // reveals[i].classList.remove("active");
+    }
+  }
+}
+
+// Reveal Elements on Scroll and load from left
+
+window.addEventListener("scroll", revealLeft);
+window.addEventListener("load", revealLeft);
+function revealLeft() {
+  const revealsLeft = document.querySelectorAll(".reveal-left");
+
+  for (let i = 0; i < revealsLeft.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = revealsLeft[i].getBoundingClientRect().top;
+    let revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      revealsLeft[i].classList.add("active");
+    } else {
+      // reveals[i].classList.remove("active");
+    }
+  }
+}
+
+// Reveal Elements on Scroll and load from right
+
+window.addEventListener("scroll", revealRight);
+window.addEventListener("load", revealRight);
+function revealRight() {
+  const revealsRight = document.querySelectorAll(".reveal-right");
+
+  for (let i = 0; i < revealsRight.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = revealsRight[i].getBoundingClientRect().top;
+    let revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      revealsRight[i].classList.add("active");
+    } else {
+      // reveals[i].classList.remove("active");
+    }
+  }
+}
+
+// Back to top button
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+  const backToTopButton = document.querySelector("#back-to-top");
+  backToTopButton.addEventListener("click", backToTop);
+
+  let windowHeight = window.innerHeight;
+  let revealtop = backToTopButton.getBoundingClientRect().top;
+  let revealpoint = 250;
+
+  // // console.log("==============");
+  console.log("(S)reveal top: " + revealtop);
+  console.log("(B)window height: " + (windowHeight - revealpoint));
+  // // console.log("==============");
+
+  if (revealtop < windowHeight - revealpoint) {
+    console.log(" ADD ACTIVE!!!");
+    backToTopButton.classList.add("active");
+  } else {
+  }
+}
+
+function backToTop() {
+  window.scrollTo(0, 0);
 }
